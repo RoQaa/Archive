28099eba787621e8157165907200b2bc9e3b9b0d
arshief321 <awyuphipfzllzspjtd@ytnhy.com>
2024-07-27 10:12:39 +0300

    file name2

--- controllers/faxController.js
+++ controllers/faxController.js
@@ -1,103 +1,95 @@
-const Fax = require('../models/faxModel')
+const Fax = require("../models/faxModel");
 const { catchAsync } = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
 // user and admin
-exports.createFax=catchAsync(async(req,res,next)=>{
-    req.body.user=req.user.id
-    const doc = await Fax.create(req.body);
-    res.status(201).json({
-        status:true,
-        message:"تم انشاء الفاكس بنجاح",
-        doc
-    })
-})
-
-
+exports.createFax = catchAsync(async (req, res, next) => {
+  req.body.user = req.user.id;
+  const doc = await Fax.create(req.body);
+  res.status(201).json({
+    status: true,
+    message: "تم انشاء الفاكس بنجاح",
+    doc,
+  });
+});
 
 //Admin
-exports.getAllFaxes=catchAsync(async(req,res,next)=>{
-    let filter={};
-    const {faxType} =req.query;
-    if(faxType) filter.faxType=faxType
-    const data = await Fax.find(filter);
-    if(!data) return next(new AppError(`No data found`,404))
-        res.status(200).json({
-            status:true,
-            data
-    })
+exports.getAllFaxes = catchAsync(async (req, res, next) => {
+  let filter = {};
+  const { faxType } = req.query;
+  if (faxType) filter.faxType = faxType;
+  const data = await Fax.find(filter);
+  if (!data) return next(new AppError(`No data found`, 404));
+  res.status(200).json({
+    status: true,
+    data,
+  });
+});
 
-})
+exports.searchByDatesAdmin = catchAsync(async (req, res, next) => {
+  const { startDate, endDate } = req.body;
 
-exports.searchByDatesAdmin=catchAsync(async(req,res,next)=>{
-    const {startDate, endDate} = req.body;
-
-    const data = await Fax.find({
-       
-        date: {
-            $gte: startDate,
-            $lte: endDate
-        }
-    });
-    if (!data||data.length===0) return next(new AppError(`No data found`, 404))
-    res.status(200).json({
-        status: true,
-        length:data.length,
-        data
-
-    })
-})
-
+  const data = await Fax.find({
+    date: {
+      $gte: startDate,
+      $lte: endDate,
+    },
+  });
+  if (!data || data.length === 0)
+    return next(new AppError(`No data found`, 404));
+  res.status(200).json({
+    status: true,
+    length: data.length,
+    data,
+  });
+});
 
 exports.updateFax = catchAsync(async (req, res, next) => {
-
-
-    const doc = await Fax.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!doc) return next(new AppError("cannot found that fax", 404));
-    res.status(200).json({
-      status: true,
-      message: "updated Successfully",
-      doc,
-    });
+  const doc = await Fax.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
   });
-  
-  exports.deleteFax = catchAsync(async (req, res, next) => {
-    await Fax.findByIdAndDelete(req.params.id);
-  
-    res.status(200).json({
-      status: true,
-      message: "deleted Successfully",
-    });
+  if (!doc) return next(new AppError("cannot found that fax", 404));
+  res.status(200).json({
+    status: true,
+    message: "updated Successfully",
+    doc,
   });
-  
+});
+
+exports.deleteFax = catchAsync(async (req, res, next) => {
+  await Fax.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({
+    status: true,
+    message: "deleted Successfully",
+  });
+});
 
 //user
-exports.getMyFaxes=catchAsync(async(req,res,next)=>{
-    const data = await Fax.find({user:req.user.id})
-    if(!data) return next(new AppError('No Data Found',404))
-        res.status(200).json({
-            status:true,
-            data
-    })
-})
+exports.getMyFaxes = catchAsync(async (req, res, next) => {
+  const data = await Fax.find({ user: req.user.id });
+  if (!data) return next(new AppError("No Data Found", 404));
+  res.status(200).json({
+    status: true,
+    data,
+  });
+});
 
-exports.searchByDatesUser=catchAsync(async(req,res,next)=>{
-    const {startDate, endDate} = req.body;
+exports.searchByDatesUser = catchAsync(async (req, res, next) => {
+  const { startDate, endDate } = req.body;
 
-    const data = await Fax.find({
-        user:req.user.id,
-        date: {
-            $gte: startDate,
-            $lte: endDate
-        }
-    });
-    if (!data||data.length===0) return next(new AppError(`No data found`, 404))
-    res.status(200).json({
-        status: true,
-        data
-
-    })
-})
+  const data = await Fax.find({
+    user: req.user.id,
+    date: {
+      $gte: startDate,
+      $lte: endDate,
+    },
+  });
+  if (!data || data.length === 0)
+    return next(new AppError(`No data found`, 404));
+  res.status(200).json({
+    status: true,
+    data,
+  });
+});
