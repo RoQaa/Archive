ed613e976d55a069f53e952c042cff7ef02e78cf
Farooq <farouk.adel931@gmail.com>
2024-07-30 00:52:11 +0300

    update

--- routes/aboutRouter.js
+++ routes/aboutRouter.js
@@ -7,7 +7,7 @@ const aboutController = require("../controllers/aboutController");
 router.use(authController.protect)
 
 router.post("/add", aboutController.createAbout);
-router.get("/", aboutController.getAbouts);
+router.get("/:id", aboutController.getAbouts);
 
 //Admin Restricts
 router.use(authController.restrictTo('admin'))
