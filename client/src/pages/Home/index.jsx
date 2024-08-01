98708d875f62c6922917460ac684e9c4f9755494
Ayman Tarek ‎ <120593869+AymanAT24@users.noreply.github.com>
2024-08-01 21:21:56 +0300

    Update index.jsx

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,21 +1,29 @@
 import { useAuth } from '@/context/Auth';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from '@/api/axios';
 import { toast, ToastContainer } from 'react-toastify';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Header from '@/layout/Header';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const { user } = useAuth();
-  const [search, setSearch] = useState('');
-  const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+  const [usernameSearch, setUsernameSearch] = useState('');
+  const [faxNumberSearch, setFaxNumberSearch] = useState('');
+  const [faxTypeSearch, setFaxTypeSearch] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
   const navigate = useNavigate();
+  const location = useLocation(); // Hook to get location object
+
   const itemsPerPage = 10;
 
-  useEffect(() => {
+  // Function to fetch data
+  const fetchData = useCallback(() => {
     const token = localStorage.getItem('userToken');
     const url = user.role === 'user' ? 'faxes/my-faxes' : 'faxes';
     axios
@@ -33,6 +41,74 @@ const Home = () => {
         toast.error('حدث خطأ ');
       });
   }, [user.role]);
+
+  // Function to fetch search results
+  const fetchSearchResults = useCallback(async () => {
+    const token = localStorage.getItem('userToken');
+    const searchParams = new URLSearchParams();
+    if (search) searchParams.append('destinationName', search);
+    if (usernameSearch) searchParams.append('username', usernameSearch);
+    if (faxNumberSearch) searchParams.append('fax_Number', faxNumberSearch);
+    if (faxTypeSearch) searchParams.append('faxType', faxTypeSearch);
+
+    let url = 'faxes/searches';
+    if (user.role === 'admin' && (startDate || endDate)) {
+      url = 'faxes/searchDateByAdmin';
+      if (startDate) searchParams.append('startDate', formatDate(startDate));
+      if (endDate) searchParams.append('endDate', formatDate(endDate));
+    }
+
+    try {
+      const res = await axios.get(`${url}?${searchParams.toString()}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setData(res.data);
+    } catch (err) {
+      console.log(err);
+      toast.error('حدث خطأ أثناء البحث');
+    }
+  }, [
+    search,
+    usernameSearch,
+    faxNumberSearch,
+    faxTypeSearch,
+    startDate,
+    endDate,
+    user.role,
+  ]);
+
+  // Refresh data on location change
+  useEffect(() => {
+    fetchData();
+  }, [location, fetchData]);
+
+  // Refresh search results on search/filter change
+  useEffect(() => {
+    if (
+      search ||
+      usernameSearch ||
+      faxNumberSearch ||
+      faxTypeSearch ||
+      startDate ||
+      endDate
+    ) {
+      fetchSearchResults();
+    } else {
+      fetchData();
+    }
+  }, [
+    search,
+    usernameSearch,
+    faxNumberSearch,
+    faxTypeSearch,
+    startDate,
+    endDate,
+    fetchSearchResults,
+    fetchData,
+  ]);
 
   const handleDelete = (id) => {
     const confirmDelete = () => {
@@ -105,10 +181,38 @@ const Home = () => {
       });
   };
 
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleUsernameSearchChange = (e) => {
+    setUsernameSearch(e.target.value);
+  };
+
+  const handleFaxNumberSearchChange = (e) => {
+    setFaxNumberSearch(e.target.value);
+  };
+
+  const handleFaxTypeSearchChange = (e) => {
+    setFaxTypeSearch(e.target.value);
+  };
+
+  const handleStartDateChange = (e) => {
+    setStartDate(e.target.value);
+  };
+
+  const handleEndDateChange = (e) => {
+    setEndDate(e.target.value);
+  };
+
+  const formatDate = (dateString) => {
+    const dat = dateString.split('-');
+    return `${dat[0]}-${dat[1]}-${dat[2]}`;
+  };
+
   const filteredData = data?.data?.filter((item) =>
     [
       item?.about?.subject?.destination?.name,
-      // item?.about?.subject?.name,
       item?.user?.username,
       item?.faxNumber,
       item?.faxType,
@@ -158,12 +262,79 @@ const Home = () => {
             </button>
           </Link>
         )}
-        <input
-          className="form-control"
-          id="search"
-          placeholder="أكتب للبحث ..."
-          onChange={(e) => setSearch(e.target.value)}
-        />
+        {user.role === 'admin' && (
+          <form className="d-flex shadow-lg" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              id="dest"
+              placeholder="أكتب للبحث بأسم الجهة"
+              aria-label="Search"
+              value={search}
+              onChange={handleSearchChange}
+            />
+            <input
+              className="form-control me-2"
+              type="search"
+              id="user"
+              placeholder="أكتب للبحث بأسم المرسل"
+              aria-label="Search"
+              value={usernameSearch}
+              onChange={handleUsernameSearchChange}
+            />
+            <input
+              className="form-control me-2"
+              type="search"
+              id="code"
+              placeholder="أكتب للبحث بكود الفاكس"
+              aria-label="Search"
+              value={faxNumberSearch}
+              onChange={handleFaxNumberSearchChange}
+            />
+            <select
+              className="form-control me-2"
+              id="faxtype"
+              value={faxTypeSearch}
+              onChange={handleFaxTypeSearchChange}
+            >
+              <option value="">اختر نوع الفاكس</option>
+              <option value="صادر">صادر</option>
+              <option value="وارد">وارد</option>
+            </select>
+          </form>
+        )}
+        <div className="dateInput d-flex ">
+          <label
+            htmlFor=""
+            className="fw-bolder me-1 d-flex justify-content-center align-items-center"
+          >
+            من
+          </label>
+          <input
+            className="form-control me-3"
+            type="date"
+            id="startDate"
+            value={startDate}
+            onChange={handleStartDateChange}
+            placeholder="أكتب للبحث بنوع الفاكس"
+            aria-label="Search"
+          />
+          <label
+            htmlFor=""
+            className="fw-bolder ms-3 me-3 d-flex justify-content-center align-items-center"
+          >
+            إلي
+          </label>
+          <input
+            className="form-control ms-3"
+            type="date"
+            id="endDate"
+            value={endDate}
+            onChange={handleEndDateChange}
+            placeholder="أكتب للبحث بنوع الفاكس"
+            aria-label="Search"
+          />
+        </div>
         {paginatedData?.length > 0 ? (
           <>
             <div className="table-responsive">
